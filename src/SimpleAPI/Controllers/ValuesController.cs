ca33538c6e92483aa741c65a56a28041da35b2e1
Alexey <alexey.nizhegorodtsev@gmail.com>
2024-11-20 21:09:53 +0000

    Initial Commit

---
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SimpleAPI.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET: api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/values/5
+        public string Get(int id)
+        {
+            string Value = "Les Jackson";
+            return Value;
+        }
+
+        // POST: api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
